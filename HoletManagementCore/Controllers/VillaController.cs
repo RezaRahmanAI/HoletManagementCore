a0d785f29e5c5d443a9045de236352f2faac6f83
Rizve Rahman Reza <rizverahman.ai@outlook.com>
2024-10-29 02:30:32 +0600

    adding villa controller

---
+++ HoletManagementCore/Controllers/VillaController.cs
@@ -0,0 +1,19 @@
+﻿using HotelManagementCore.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HoletManagementCore.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var villas = _context.Villas.ToList();
+            return View();
+        }
+    }
+}
